3734798aa3c48d889db8717a832a2e12f92b7614
Sujil S <sujil192110912@gmail.com>
2025-12-24 09:49:03 +0530

    Data Validation Using Pydantic

--- pydantic.py
+++ pydantic.py
@@ -1,18 +1,32 @@
 from pydantic import BaseModel
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
 
-    patient_name : str
-    patient_age : int
+    name : str
+    age : int
+    weight : float
+    height : float
+    bmi : float
+    married : bool = False
+    allergies : Optional[List[str]] = None
+    contact_details : Dict[str, str]
 
-def insert_patient_data(patient : Patient):
+def update_patient_data(patient : Patient):
     
-    print(patient.patient_name)
-    print(patient.patient_age)
+    print(patient.name)
+    print(patient.age)
+    print(patient.weight)
+    print(patient.height)
+    print(patient.bmi)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
 
     return "Patient data inserted successfully" 
 
-patient_info = {'patient_name' : "Sujil S", "patient_age" : 25}
+patient_info = {'name' : "Sujil S", "age" : 25, 'weight' : 75, 'height' : 175, 'bmi' : 25, 'contact_details' : {'phone' : '9876543210', 'email' : 'EMAIL'} }
+
 patient_1 = Patient(**patient_info)
 
-insert_patient_data(patient_1)
+update_patient_data(patient_1)
